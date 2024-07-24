33714c5c536dffcd2f4b6a385bbb0797db10bfb5
sungjun son <o_nj_u@naver.com>
2024-07-24 21:44:13 +0900

    코드 기본설정 / textField

--- textFieldProject/ViewController.swift
+++ textFieldProject/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
 
+    func setup() {
+        view.backgroundColor = UIColor.gray
+        // ⌥ + 클릭을 생활화 하면 좋음!
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .next
+    }
+
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
 
 }
 
